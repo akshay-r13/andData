cebb45bf10da959f99a19cffa016e493d068778f
AkshayRaman97 <akshay.raman97@gmail.com>
2019-08-16 08:40:02 +0530

    Updates

---
+++ precache-manifest.bc1fc70567ab1f26d4ee8c552297fb72.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ef60fe42abaf905d67f38ffcfd8f631",
+    "url": "/site-anddata/index.html"
+  },
+  {
+    "revision": "10fde4f92b7ce151b77c",
+    "url": "/site-anddata/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "65b8ccc94998a55a6d18",
+    "url": "/site-anddata/static/css/main.ab145a2b.chunk.css"
+  },
+  {
+    "revision": "10fde4f92b7ce151b77c",
+    "url": "/site-anddata/static/js/2.c37d6174.chunk.js"
+  },
+  {
+    "revision": "65b8ccc94998a55a6d18",
+    "url": "/site-anddata/static/js/main.ef0a1309.chunk.js"
+  },
+  {
+    "revision": "9afb27dfe0591bce22fe",
+    "url": "/site-anddata/static/js/runtime~main.c61c68ac.js"
+  }
+]);
